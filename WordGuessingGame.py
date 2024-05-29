c3e236385e27a05f922509f833761da7b6a9a159
JunQi123-del <tanjunqi47@gmail.com>
2024-05-29 23:47:28 +0800

    WordGuessingGameProject

---
+++ WordGuessingGame.py
@@ -0,0 +1,41 @@
+import random
+
+userName = input("What is your name?: ")
+numberOfGuess = 12
+guessLeft = numberOfGuess
+listOfWords = ["Morning","Day","Night"]
+secretWord = random.choice(listOfWords)
+#secretWord = "Morning"
+hiddenWord = "*"*len(secretWord)
+secretWordFound = False
+guessCorrect = False
+
+print("Guess the word: ",hiddenWord)
+while(guessLeft>0):
+    userGuess = input("Guess a Alphabet: ")
+    for i, char in enumerate(secretWord):
+        if userGuess==char:
+            hiddenWordList = list(hiddenWord)
+            hiddenWordList[i] = userGuess
+            hiddenWord = "".join(hiddenWordList)
+            guessCorrect = True
+
+    if hiddenWord.find("*")==-1:
+        print("Congratulation ",userName,", You have found the secret word!")
+        secretWordFound = True
+        break
+    
+    guessLeft-=1
+
+    if guessCorrect:
+        print("Good Guess! getting closer to the word!: ",hiddenWord)
+    else:
+        print("Wrong Guess! Don't give up! ",guessLeft," more tries!: ",secretWord)   
+    guessCorrect = False
+
+
+if(secretWordFound==False):
+    print("The secret word is: ",secretWord," , Better luck next time")
+    
+
+
